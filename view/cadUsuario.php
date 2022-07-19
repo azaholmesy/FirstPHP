702af68b15801ca390f6e977296b75bd60f4a2ca
Felipe Correa Soares <182100521@senacinformatica.br>
2022-07-19 16:21:44 -0300

    creating cadUsuario.php

---
+++ view/cadUsuario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php 
+    require_once '../controller/CUsuario.php';
+    $cadUsuario = new CUsuario;
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php 
+        
+        ?>
+    </body>
+</html>
