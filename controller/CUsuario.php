885d74c961edbba2dab1f474a6d79d2ed0cad89c
Felipe Correa Soares <182100521@senacinformatica.br>
2022-07-21 14:57:37 -0300

    fix connection problem

--- controller/CUsuario.php
+++ controller/CUsuario.php
@@ -1,20 +1,30 @@
-<?php 
+<?php
 
- class CUsuario {
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
 
+/**
+ * Description of CUsuario
+ *
+ * @author jbferraz
+ */
+class CUsuario {
+
+    //put your code here
     public function inserir() {
-        if(isset($_POST['salvar'])){
+        if (isset($_POST['salvar'])) {
             $nome = $_POST['nome'];
             $usuario = $_POST['usuario'];
             $senha = $_POST['senha'];
-            
+
             $pdo = require_once '../pdo/Connection.php';
-            $sql = 'insert into crudpessoa.usuario values (null,?,?,?)';
+            $sql = "insert into usuario values (null,?,?,?)";
             $sth = $pdo->prepare($sql);
             $sth->bindParam(1, $nome, PDO::PARAM_STR);
             $sth->bindParam(2, $usuario, PDO::PARAM_STR);
             $sth->bindParam(3, $senhaEc, PDO::PARAM_STR);
-
             $senhaEc = password_hash($senha, PASSWORD_DEFAULT);
             $sth->execute();
             unset($sth);
@@ -22,9 +32,9 @@
         }
     }
 
-    public function getUsuarios(){
-        $pdo = require_once './pdo/Connection.php';
-        $sql = 'select idUsuario, nomeUsuario, usuario from usuario';
+    public function getUsuarios() {
+        $pdo = require_once '../pdo/Connection.php';
+        $sql = "select idUsuario, nomeUsuario, usuario from usuario";
         $sth = $pdo->prepare($sql);
         $sth->execute();
         $result = $sth->fetchAll();
@@ -33,23 +43,24 @@
         return $result;
     }
 
-    public function deletar(){
-        if(isset($_POST['deletar'])){
-            $id = (int)$_POST['idUsuario'];
-            $pdo = require_once './pdo/Connection.php';
-            $sql = 'delete from usuario where idUsuario = ?';
+    public function deletar() {
+        if (isset($_POST['deletar'])) {
+            $id = (int) $_POST['idUsuario'];
+            $pdo = require_once '../pdo/Connection.php';
+            $sql = "delete from usuario where idUsuario = ?";
             $sth = $pdo->prepare($sql);
             $sth->bindParam(1, $id, PDO::PARAM_INT);
             $sth->execute();
             unset($sth);
             unset($pdo);
-            header('Refresh: 0');
+            header("Refresh: 0");
         }
     }
 
     public function getUsuarioById($id) {
-        $pdo = require_once './pdo/Connection.php';
-        $sql = 'select idUsuario, nomeUsuario, usuario from usuario where idUsuario = ?';
+        $pdo = require_once '../pdo/Connection.php';
+        $sql = "select idUsuario, nomeUsuario, usuario "
+                . "from usuario where idUsuario = ?";
         $sth = $pdo->prepare($sql);
         $sth->bindParam(1, $id, PDO::PARAM_INT);
         $sth->execute();
@@ -59,13 +70,12 @@
         return $result;
     }
 
-    public function trocaSenha(){
-        if(isset($_POST['trocaSenha'])){
+    public function trocaSenha() {
+        if (isset($_POST['trocaSenha'])) {
             $idUsuario = $_POST['idUsuario'];
             $novaSenha = $_POST['novaSenha'];
-
-            $pdo = require_once './pdo/Connection.php';
-            $sql = 'update usuario set senha = ? where isUsuario = ?';
+            $pdo = require_once '../pdo/Connection.php';
+            $sql = "update usuario set senha = ? where idUsuario = ?";
             $sth = $pdo->prepare($sql);
             $sth->bindParam(1, $senhaPH, PDO::PARAM_STR);
             $senhaPH = password_hash($novaSenha, PASSWORD_DEFAULT);
@@ -73,7 +83,8 @@
             $sth->execute();
             unset($sth);
             unset($pdo);
-            header('Location: usuario.php');
+            header("Location: usuario.php");
         }
     }
- }
+
+}
