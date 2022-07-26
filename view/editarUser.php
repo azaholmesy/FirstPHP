5464e8485b732545e683ebb6c5bc06b412c868a5
Felipe Correa Soares <182100521@senacinformatica.br>
2022-07-26 17:19:34 -0300

    wip

---
+++ view/editarUser.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php
+    $id = null;
+    if(isset($_POST['editar'])) {
+
+    }
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
