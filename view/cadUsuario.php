8917bf27b0bc9548d9f6f537eb63faf2f7428a37
Felipe Correa Soares <182100521@senacinformatica.br>
2022-07-26 14:19:29 -0300

    creating listaUsuario

--- view/cadUsuario.php
+++ view/cadUsuario.php
@@ -34,37 +34,7 @@
             <input type="button" value="Voltar"
             onclick="location.href='../index.php'"/>
             <input type="button" value="Listar Usuários"
-            onclick="document.getElementById('lista').style.visibility='visible'"/>
+            onclick="location.href='./listaUsuario.php'"/>
         </form>
-        <div id="lista" style="visibility: hidden;">
-            <h2>Lista Usuários</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome Usuário</th>
-                        <th>Usuário</th>
-                        <th>Funções</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach ($listaUsers as $user):
-                    ?>
-                    <tr>
-                        <td><?php echo $user['idUsuario']; ?></td>
-                        <td><?php echo $user['nomeUsuario']; ?></td>
-                        <td><?php echo $user['usuario']; ?></td>
-                        <td>-----</td>
-                    </tr>
-                    <?php 
-                        endforeach;
-                    ?>
-                </tbody>
-            </table>
-            <br/><br/>
-            <input type="button" value="Ocultar Lista"
-            onclick="document.getElementById('lista').style.visibility='hidden'"/>
-        </div>
     </body>
 </html>
---
+++ view/listaUsuario.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<?php 
+    require_once '../controller/CUsuario.php';
+    $cadUsuario = new CUsuario;
+    $listaUsers = $cadUsuario->getUsuarios();
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lista de Usuários</title>
+    </head>
+    <body>
+        <div id="lista">
+            <h2>Lista Usuários</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome Usuário</th>
+                        <th>Usuário</th>
+                        <th>Funções</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                            foreach ($listaUsers as $user):
+                    ?>
+                    <tr>
+                        <td><?php echo $user['idUsuario']; ?></td>
+                        <td><?php echo $user['nomeUsuario']; ?></td>
+                        <td><?php echo $user['usuario']; ?></td>
+                        <td>-----</td>
+                    </tr>
+                    <?php 
+                            endforeach;
+                    ?>
+                </tbody>
+            </table>
+            <br/><br/>
+            <input type="button" value="Voltar"
+            onclick="location.href='./cadUsuario.php'"/>
+            <input type="button" value="Voltar Para a Página Inicial"
+            onclick="location.href='../index.php'"/>
+            </div>
+    </body>
+</html>
